ae648579f67ca331d2816ac977fc101860c51887
atheodosiou <anastasios.theodosiou@gmail.com>
2019-03-03 18:17:40 +0200

    Complete reading all necessary data

--- src/main/scala/Supervised/App.scala
+++ src/main/scala/Supervised/App.scala
@@ -22,7 +22,11 @@ object App {
       val executionCores = args(0).toInt
       val numPartitions = args(1).toInt
       val model = args(2).toString
+
+      //Values
       val nodes_csv_file_path = "resources/graph/nodes.csv"
+      val training_set_file_path = "resources/supplementary_files/training_set.txt"
+      val test_set_file_path = "resources/supplementary_files/test_set.txt"
 
       println("App is running "+model+" with "+executionCores+" cores and "+numPartitions+" partitions.\n")
 
@@ -42,7 +46,11 @@ object App {
           StructField("abstract", StringType) :: Nil
       )
 
-      println("Creating nodeDF from "+nodes_csv_file_path+"file using StructType.\n")
+      import spark.implicits._
+
+      //=========================================> Reading Data Sets <=========================================
+
+      println("Creating nodeDF from "+nodes_csv_file_path+" file using StructType.\n")
       val nodeDf = spark.read.option("header", false).schema(schemaStruct)
         .csv(nodes_csv_file_path)
         .repartition(numPartitions)
@@ -51,8 +59,44 @@ object App {
       val totalNodes = nodeDf.count()
 
       println("Repartitioning nodesDF into "+nodeDf.rdd.getNumPartitions+" partitions\n")
-      println("NodesDf's total entries:"+totalNodes)
-    }
+      println("NodesDf's total entries:"+totalNodes+"\n")
 
+      println("Showing sample of nodesDF...\n")
+      nodeDf.show(5,false)
+
+      println("Reading training data from "+ training_set_file_path+" file...\n")
+      val trainingDF = sc.textFile(training_set_file_path).map(line =>{
+        val fields = line.split(" ")
+        (fields(0),fields(1), fields(2).toInt)
+      }).toDF("srcId","dstId","label").repartition(numPartitions)
+        .cache()
+
+      val totalTrainingEdges = trainingDF.count()
+
+      println("Repartitioning trainingDF into "+trainingDF.rdd.getNumPartitions+" partitions\n")
+      println("TrainingDF's total entries:"+totalTrainingEdges+"\n")
+
+      println("Showing sample of trainingDF...\n")
+      trainingDF.show(5,false)
+
+      println("Reading test data from "+test_set_file_path +" file...\n")
+      val testDF = sc.textFile(test_set_file_path).map(line =>{
+        val fields = line.split(" ")
+        (fields(0),fields(1))
+      }).toDF("srcId","dstId").repartition(numPartitions)
+        .cache()
+
+      val totalTestEdges = testDF.count()
+
+      println("Repartitioning testDF into "+testDF.rdd.getNumPartitions+" partitions\n")
+      println("TestDF's total entries:"+totalTestEdges +"\n")
+
+      println("Showing sample of testDF...\n")
+      testDF.show(5,false)
+
+      //=======================================================================================================
+
+      //=========================================> Reading Data Sets <=========================================
+    }
   }
 }
